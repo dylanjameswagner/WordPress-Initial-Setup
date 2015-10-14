90cc685afc4e1b0e9679b7ed02764330923d5166
Dylan James Wagner <dylan@teamcolab.com>
2015-10-14 16:48:27 -0400

    Update redirects.php

--- redirects.php
+++ redirects.php
@@ -1,15 +1,19 @@
 <?php
-
-/* https://pantheon.io/docs/articles/sites/code/redirect-incoming-requests/ */
-// use keys with no trailing slash for with/without compatibility
-
-$request = rtrim($_SERVER['REQUEST_URI'],'/\\'); // remove trailing slash
+/**
+ * 301 Redirects
+ *
+ * @link https://pantheon.io/docs/articles/sites/code/redirect-incoming-requests/
+ *
+ * Use array keys with no trailing slash for compatibility with or without.
+ */
+$request = rtrim( $_SERVER['REQUEST_URI'], '/\\' ); // remove trailing slash
 $redirects = array(
-	'/old-page' => '/new-page/',
+    '/old-page' => '/new-page/',
+);
 );
 
-if (array_key_exists($request,$redirects)) :
-	header('HTTP/1.0 301 Moved Permanently');
-	header('Location: '.$redirects[$request]);
-	exit();
+if ( array_key_exists( $request, $redirects ) ) :
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: ' . $redirects[ $request ]);
+    exit();
 endif;
