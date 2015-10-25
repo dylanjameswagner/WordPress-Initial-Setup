7dfc7f2ef15a07509db0e11791545babc9b4f829
Dylan James Wagner <dylan@teamcolab.com>
2015-10-25 18:25:26 -0400

    Update redirects.php

--- redirects.php
+++ redirects.php
@@ -10,7 +10,6 @@ $request = rtrim( $_SERVER['REQUEST_URI'], '/\\' ); // remove trailing slash
 $redirects = array(
     '/old-page' => '/new-page/',
 );
-);
 
 if ( array_key_exists( $request, $redirects ) ) :
     header('HTTP/1.0 301 Moved Permanently');
